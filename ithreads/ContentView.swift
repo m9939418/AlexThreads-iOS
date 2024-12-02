126765acbf2dac39478be338e741396191efe9ba
AlexYang <m9939418@gmail.com>
2024-12-02 18:18:35 +0800

    Initial Commit

---
+++ ithreads/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ithreads
+//
+//  Created by Alex YANG 楊明仁 on 2024/12/2.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ithreads/ithreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ithreadsApp.swift
+//  ithreads
+//
+//  Created by Alex YANG 楊明仁 on 2024/12/2.
+//
+
+import SwiftUI
+
+@main
+struct ithreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
