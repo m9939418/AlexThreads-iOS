df4afb5c7aa128f0a72738e4eaef33867e3b786d
AlexYang <m9939418@gmail.com>
2024-12-03 10:32:48 +0800

    #1 project setup

---
+++ ithreads/App/ithreadsApp.swift
@@ -0,0 +1,29 @@
+//
+//  ithreadsApp.swift
+//  ithreads
+//
+//  Created by Alex YANG 楊明仁 on 2024/12/2.
+//
+
+import SwiftUI
+import FirebaseCore
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+  func application(_ application: UIApplication,
+                   didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    FirebaseApp.configure()
+
+    return true
+  }
+}
+
+@main
+struct ithreadsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+    
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
